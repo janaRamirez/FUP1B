f14e724b85bc415adb4bfbb051f265d66b03a047
janaRamirez <lizz5259@hotmail.com>
2015-10-27 14:38:40 -0600

    juana

---
+++ examenrecuperacionjuana.c
@@ -0,0 +1,164 @@
+#include<stdio.h>
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int main()
+{
+	printf("ingresa 10 numeros\n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	scanf("%d",&d);
+	scanf("%d",&e);
+	scanf("%d",&f);
+	scanf("%d",&g);
+	scanf("%d",&h);
+	scanf("%d",&i);
+	scanf("%d",&j);
+	if(a>=b && a>=c && a>=d && a>=e && a>=f && a>=g && a>=h && a>=i && a>=j)
+	{
+		printf("el mayor es:%d\n",a);
+	}
+	else
+	{
+		if(b>=a && b>=c && b>=d && b>=e && b>=f && b>=g && b>=h && b>=i && b>=j)
+		{
+			printf("el mayor es:%d\n",b);
+		}
+		else
+		{
+			if(c>=a && c>=b && c>=d && c>=e && c>=f && c>=g && c>=h && c>=i && c>=j)
+			{
+				printf("el mayor es:%d\n",c);
+			}
+			else
+			{
+				if(d>=a && d>=b && d>=c && d>=e && d>=f && d>=g && d>=h && d>=i && d>=j)
+				{
+					printf("el mayor es:%d\n",d);
+				}
+				else
+				{
+					if(e>=a && e>=b && e>=c && e>=d && e>=f && e>=g && e>=h && e>=i && e>=j)
+					{
+						printf("el mayor es:%d\n",e);
+					}
+					else
+				    {
+					if(f>=a && f>=b && f>=c && f>=d && f>=e && f>=g && f>=h && f>=i && f>=j)
+					{
+						printf("el mayor es:%d\n",f);
+					}
+					else
+				    {
+					if(g>=a && g>=b && g>=c && g>=d && g>=f && g>=e && g>=h && g>=i && g>=j)
+					{
+						printf("el mayor es:%d\n",g);
+					}
+					else
+				{
+					if(h>=a && h>=b && h>=c && h>=d && h>=f && h>=g && h>=e && h>=i && h>=j)
+					{
+						printf("el mayor es:%d\n",h);
+					}
+					else
+				{
+					if(i>=a && i>=b && i>=c && i>=d && i>=f && i>=g && i>=h && i>=e && i>=j)
+					{
+						printf("el mayor es:%d\n",i);
+					}
+					else
+				{
+					if(j>=a && j>=b && j>=c && j>=d && j>=f && j>=g && j>=h && j>=i && j>=e)
+					{
+						printf("el mayor es:%d\n",j);
+					}
+				}
+			}
+		}
+	}
+}
+}
+}
+}
+}
+
+if(a<=b && a<=c && a<=d && a<=e && a<=f && a<=g && a<=h && a<=i && a<=j)
+	{
+		printf("el menor es:%d",a);
+	}
+	else
+	{
+		if(b<=a && b<=c && b<=d && b<=e && b<=f && b<=g && b<=h && b<=i && b<=j)
+		{
+			printf("el menor es:%d",b);
+		}
+		else
+		{
+			if(c<=a && c<=b && c<=d && c<=e && c<=f && c<=g && c<=h && c<=i && c<=j)
+			{
+				printf("el menor es:%d",c);
+			}
+			else
+			{
+				if(d<=a && d<=b && d<=c && d<=e && d<=f && d<=g && d<=h && d<=i && d<=j)
+				{
+					printf("el menor es:%d",d);
+				}
+				else
+				{
+					if(e<=a && e<=b && e<=c && e<=d && e<=f && e<=g && e<=h && e<=i && e<=j)
+					{
+						printf("el menor es:%d",e);
+					}
+					else
+				{
+					if(f<=a && f<=b && f<=c && f<=d && f<=e && f<=g && f<=h && f<=i && f<=j)
+					{
+						printf("el menor es:%d",f);
+					}
+					else
+				{
+					if(g<=a && g<=b && g<=c && g<=d && g<=f && g<=e && g<=h && g<=i && g<=j)
+					{
+						printf("el menor es:%d",g);
+					}
+					else
+				{
+					if(h<=a && h<=b && h<=c && h<=d && h<=f && h<=e && h<=g && h<=i && h<=j)
+					{
+						printf("el menor es:%d",h);
+					}
+					else
+				{
+					if(i<=a && i<=b && i<=c && i<=d && i<=f && i<=e && i<=g && i<=h && i<=j)
+					{
+						printf("el menor es:%d",i);
+					}
+					else
+				{
+					if(j<=a && j<=b && j<=c && j<=d && j<=f && j<=e && j<=h && j<=g && j<=i)
+					{
+						printf("el menor es:%d",j);
+					}
+			}
+			}
+		}
+	}
+}
+}
+}
+}
+}
+}
+
+
+
+
